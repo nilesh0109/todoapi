5e909e9926858b7ac07af6a987aaa92881b5bef4
Nilesh Vijayrania <nilesh0109@gmail.com>
2017-02-12 10:42:39 +0400

    committing pending changes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var bodyparser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3002;
-
+var nextItemId = 1;
 var todos = [{
     id: 1,
     description: 'Meet Mom for lunch',
@@ -11,12 +12,15 @@ var todos = [{
     description: 'Go to market today',
     completed: false
 }];
+todos = [];
 
 Array.prototype.getEleById = function(id) {
     return this.filter(function(v) {
         return v.id === parseInt(id);
     })[0];
 }
+
+app.use(bodyparser.json());
 
 app.get('/', function(req, res) {
     res.send('TODO API ROOT');
@@ -36,6 +40,14 @@ app.get('/todo/:id', function(req, res) {
         res.json(ele);
     else
         res.status(404).send();
+});
+
+app.post('/todos', function(req, res) {
+    var body = req.body;
+
+    body.id = nextItemId++;
+    todos.push(body);
+    res.json(body.id);
 });
 
 // GET Todo request /
