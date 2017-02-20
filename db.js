456fc7f8873c68672fd5e7637e2af0437fba0d60
Nilesh Vijayrania <nilesh0109@gmail.com>
2017-02-20 14:11:24 +0400

    adding postgres for heroku deployment

--- db.js
+++ db.js
@@ -1,9 +1,18 @@
 var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+if (env === 'production') {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        'dialect': 'postgres'
+    });
+} else {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/todoapi.sqlite'
+    });
 
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect': 'sqlite',
-    'storage': __dirname + '/data/todoapi.sqlite'
-});
+}
+
 
 var db = {};
 db.sequelize = sequelize;
